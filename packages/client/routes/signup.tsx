25b0462105047ba43f6da7cc51e49deb8534ccf2
Patrick Glendon McCullough <p@mccullo.ug>
2024-04-18 13:24:55 -0400

    signup component

--- packages/client/routes/signup.tsx
+++ packages/client/routes/signup.tsx
@@ -1,9 +1,11 @@
 import { Header } from "../components/Header/Header.tsx";
+import { SignUp } from "../components/SignUp/SignUp.tsx";
 
 export default function Signup() {
   return (
     <>
       <Header />
+      <SignUp />
     </>
   );
 }
